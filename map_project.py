6a3d82c39dfc25ce6a794996addc0e8fc860bd43
James Han <jhan@cibotechnologies.com>
2022-05-06 00:40:06 +0800

    Add potentially geopandas work around for streamlit, don;t know if it will work or not yet.

--- map_project.py
+++ map_project.py
@@ -1,3 +1,11 @@
+import conda
+import os
+
+conda_file_dir = conda.__file__
+conda_dir = conda_file_dir.split('lib')[0]
+proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
+os.environ["PROJ_LIB"] = proj_lib
+
 
 import pandas as pd # library for data analsysis
 import json # library to handle JSON files
